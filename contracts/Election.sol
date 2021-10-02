e9f3e2cbffd0902a5d8319b74c1ac82fd7fdcf21
Akash-Kumar-Sikarwar <akash.sky.heaven@gmail.com>
2021-10-02 16:48:33 +0530

    complete prototype

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    // store canditate 
+    // read canditate
+   string public candidate;
+    // contructor
+    function Election() public{
+        candidate = "candidate1";
+    }
+
+    
+}
